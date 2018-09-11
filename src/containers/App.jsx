62fbe7e1b5d7f318f30105c7d733cffd7e7851e0
F1r3byte <f1r3byte@yandex.com>
2018-09-11 23:54:09 +0200

    made various changes and put module in app

---
+++ src/containers/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Navbar from "../components/layout/Navbar";
+import "./App.css";
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Navbar />
+			</div>
+		);
+	}
+}
+
+export default App;
