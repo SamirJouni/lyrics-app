f0cae342e2f203740ad6e0ba3822a40143c421ad
F1r3byte <f1r3byte@yandex.com>
2018-09-11 23:40:23 +0200

    added font awesome

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
