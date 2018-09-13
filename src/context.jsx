54445ee4fd452494285ea02bc2349fd9f60c9947
F1r3byte <f1r3byte@yandex.com>
2018-09-13 23:31:58 +0200

    now setting state with api data

--- src/context.jsx
+++ src/context.jsx
@@ -5,17 +5,14 @@ const Context = React.createContext();
 export class Provider extends Component {
 
 	state = {
-		track_list: [
-			{track: {track_name: 'abc'}},
-			{track: {track_name: '123'}},
-		],
+		track_list: [],
 		heading: 'Top 10 Tracks'
 	}
 
 	componentDidMount () {
 		fetch(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=de&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
 		.then(response => response.json())
-		.then(data => console.log(data))
+		.then(data => this.setState({track_list: data.message.body.track_list}))
 		.catch(error => console.log(error));
 	}
 
