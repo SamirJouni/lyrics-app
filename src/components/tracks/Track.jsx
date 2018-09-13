416d76584112742fedee113095051e103e75857d
F1r3byte <f1r3byte@yandex.com>
2018-09-13 22:06:02 +0000

    fixed bug

--- src/components/tracks/Track.jsx
+++ src/components/tracks/Track.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Track = props => {
 	const { track } = props;
+	console.log(track);
 	return (
 		<div className="col-md-6">
 			<div className="card mb-4 shadow-sm">
