45016772c08d346f5dd3c89d0cd231ec90b4b553
F1r3byte <f1r3byte@yandex.com>
2018-09-14 23:57:14 +0200

    removed import for old fontawesome

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.min.css';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
