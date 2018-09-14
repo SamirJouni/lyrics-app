99ea74644687d01258bec160f603abe1fb0ba867
F1r3byte <f1r3byte@yandex.com>
2018-09-14 23:47:59 +0200

    showing song name

--- src/components/tracks/Track.jsx
+++ src/components/tracks/Track.jsx
@@ -8,6 +8,9 @@ const Track = props => {
 			<div className="card mb-4 shadow-sm">
 				<div className="card-body">
 					<h5>{track.artist_name}</h5>
+					<p className="card-text">
+						<strong><i className="fas fa-play"></i> Track</strong>: {track.track_name}
+					</p>
 				</div>
 			</div>
 		</div>
