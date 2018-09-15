297d8282523a4f69e8a6374d7fdba3c4974684f5
F1r3byte <f1r3byte@yandex.com>
2018-09-16 00:04:35 +0200

    added view lyrics button

--- src/components/tracks/Track.jsx
+++ src/components/tracks/Track.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Track = props => {
 	const { track } = props;
@@ -9,9 +10,21 @@ const Track = props => {
 				<div className="card-body">
 					<h5>{track.artist_name}</h5>
 					<p className="card-text">
-						<strong><i className="fas fa-play"></i> Track</strong>: {track.track_name} <br />
-						<strong><i className="fas fa-compact-disc"></i> Album</strong>: {track.album_name}
+						<strong>
+							<i className="fas fa-play" /> Track
+						</strong>
+						: {track.track_name} <br />
+						<strong>
+							<i className="fas fa-compact-disc" /> Album
+						</strong>
+						: {track.album_name}
 					</p>
+					<Link
+						to={`lyrics/track/${track.track_id}`}
+						className="btn btn-dark btn-block"
+					>
+						<i class="fas fa-chevron-right" /> View Lyrics
+					</Link>
 				</div>
 			</div>
 		</div>
