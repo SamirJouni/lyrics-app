25cdbcd39ac5bab1a00e0f3f89075bdee42a3a0e
F1r3byte <f1r3byte@yandex.com>
2018-09-16 23:43:32 +0200

    plugged lyrics in app

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "../context";
 import Navbar from "../components/layout/Navbar";
 import Index from "../components/layout/Index";
+import Lyrics from "../components/tracks/Lyrics";
 import "./App.css";
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
 						<div className="container">
 							<Switch>
 								<Route exact path="/" component={Index} />
+								<Route exact path="/lyrics/track/:id" component={Lyrics} />
 							</Switch>
 						</div>
 					</React.Fragment>
