63ffeddaeb614b1be8de4deb0ec30361a0210883
F1r3byte <f1r3byte@yandex.com>
2018-09-18 23:55:10 +0200

    added text to card

--- src/components/tracks/Search.jsx
+++ src/components/tracks/Search.jsx
@@ -9,9 +9,20 @@ class Search extends Component {
 
 	render() {
 		return (
-			<div>
-
-			</div>
+			<Consumer>
+				{
+					value => {
+						return (
+							<div className="card card-body mb-4 p-4">
+								<h1 className="display-4 text-center">
+									<i className="fas fa-music mr-2"></i> What Song Are You Looking For ?
+								</h1>
+								<p className="lead text-center">Search For Your Song And View It's Lyrics</p>
+							</div>
+						);
+					}
+				}
+			</Consumer>
 		 );
 	}
 }
