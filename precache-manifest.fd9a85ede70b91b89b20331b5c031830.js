dad7b31e12217a0ee569f5d303a95771d03a0e73
F1r3byte <f1r3byte@yandex.com>
2019-04-23 23:53:33 +0200

    Updates

---
+++ precache-manifest.fd9a85ede70b91b89b20331b5c031830.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5544c4818cdb4f6c1cbbfa052c7e2cb",
+    "url": "/lyrics-app/index.html"
+  },
+  {
+    "revision": "3f12b3f9728ab1f0c8dd",
+    "url": "/lyrics-app/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "0d9a6de1ce92b9e262a0",
+    "url": "/lyrics-app/static/css/main.2a7c4e9e.chunk.css"
+  },
+  {
+    "revision": "3f12b3f9728ab1f0c8dd",
+    "url": "/lyrics-app/static/js/2.917a7fee.chunk.js"
+  },
+  {
+    "revision": "0d9a6de1ce92b9e262a0",
+    "url": "/lyrics-app/static/js/main.6506c24b.chunk.js"
+  },
+  {
+    "revision": "5bc2783d88630108a3e1",
+    "url": "/lyrics-app/static/js/runtime~main.f14bbab0.js"
+  },
+  {
+    "revision": "f3ac8eb8aab1477270cf64a6157e80c2",
+    "url": "/lyrics-app/static/media/spinner.f3ac8eb8.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/lyrics-app/precache-manifest.227e80aeca1198ab97dc648787a12ba7.js"
+  "/lyrics-app/precache-manifest.fd9a85ede70b91b89b20331b5c031830.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/lyrics-app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lyrics-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
