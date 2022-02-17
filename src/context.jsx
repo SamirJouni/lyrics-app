313c5ca3d0472f8dbfc20c45934cc192f556f7a1
F1r3byte <f1r3byte@yandex.com>
2022-02-17 12:36:42 +0100

    fixed no cors

--- src/context.jsx
+++ src/context.jsx
@@ -28,7 +28,7 @@ export class Provider extends Component {
 	}
 
 	componentDidMount () {
-		fetch(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=de&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+		fetch(`https://crossorigin.me/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=de&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
 		.then(response => response.json())
 		.then(data => this.setState({track_list: data.message.body.track_list}))
 		.catch(error => console.log(error));
